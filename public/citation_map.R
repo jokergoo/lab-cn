dd1f64df932d7f748dc0ba9735f2f3b1bbc677e3
Zuguang Gu <jokergoo@gmail.com>
2026-03-03 09:32:13 +0800

    update

--- public/citation_map.R
+++ public/citation_map.R
@@ -1,4 +1,4 @@
-ln = readLines(url("https://www.webofscience.com/wos-researcher/dashboard/citation-map/?task_id=3a3d0b96-3f95-4aa3-91fe-954ddd61c73e"))
+ln = readLines(url("https://webofscience.clarivate.cn/wos-researcher/dashboard/citation-map/?task_id=e235cfea-f097-4282-8887-d41d4618b842"))
 ln[1] = paste0("citation=", ln[1])
 
 tmp = tempfile()
